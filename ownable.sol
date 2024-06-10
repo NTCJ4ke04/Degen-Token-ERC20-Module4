a73e2aab2578aeee59609eff6d394d3e8aadffc3
NTCJ4ke04 <127911123+NTCJ4ke04@users.noreply.github.com>
2024-06-10 14:18:11 +0800

    Update ownable.sol

--- ownable.sol
+++ ownable.sol
@@ -1,1 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Ownable {
+    address public owner;
+    address public pendingOwner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    event OwnershipTransferCancelled(address indexed previousPendingOwner);
+
+    constructor() {
+        owner = msg.sender;
+        emit OwnershipTransferred(address(0), owner);
+    }
+
+    modifier onlyOwner() {
+        require(owner == msg.sender, "Ownable: caller is not the owner");
+        _;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        pendingOwner = newOwner;
+    }
+
+    function cancelTransferOwnership() public onlyOwner {
+        require(pendingOwner != address(0), "Ownable: no pending owner to cancel");
+        emit OwnershipTransferCancelled(pendingOwner);
+        pendingOwner = address(0);
+    }
+
+    function claimOwnership() public {
+        require(msg.sender == pendingOwner, "Ownable: caller is not the pending owner");
+        emit OwnershipTransferred(owner, pendingOwner);
+        owner = pendingOwner;
+        pendingOwner = address(0);
+    }
+}
 
